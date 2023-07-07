517a50a313fa17d5d4b6a007f64b52e425926a06
gmaralan <gmartinialan@gmail.com>
2023-07-07 06:12:15 -0400

    chore: fases class implementation

---
+++ src/main/java/segundoteste/fases/Aprovados.java
@@ -0,0 +1,9 @@
+package segundoteste.fases;
+
+import segundoteste.candidatos.Candidato;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aprovados extends AbsFase {
+}
---
+++ src/main/java/segundoteste/fases/Qualificados.java
@@ -0,0 +1,4 @@
+package segundoteste.fases;
+
+public class Qualificados extends AbsFase {
+}
