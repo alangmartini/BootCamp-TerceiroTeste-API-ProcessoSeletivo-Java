227c3ca13e31e19cc540a2076416cec3b7e26a55
gmaralan <gmartinialan@gmail.com>
2023-07-07 10:17:57 -0400

    docs: javadocs for interfaces.

--- src/main/java/segundoteste/IProcessManager.java
+++ src/main/java/segundoteste/IProcessManager.java
@@ -2,15 +2,52 @@ package segundoteste;
 
 import java.util.List;
 
+/**
+ * IProcessManager é uma interface que define os métodos que devem ser implementados pelo gerenciador do processo de seleção.
+ */
 public interface IProcessManager {
+
+    /**
+     * Este método deve iniciar o processo para um determinado candidato, dado seu nome.
+     *
+     * @param nome O nome do candidato.
+     * @return O código do candidato.
+     */
     public int iniciarProcesso(String nome);
+
+    /**
+     * Este método deve marcar uma entrevista para um determinado candidato, dado seu código.
+     *
+     * @param codCandidato O código do candidato.
+     */
     public void marcarEntrevista(int codCandidato);
 
+    /**
+     * Este método deve desqualificar um determinado candidato do processo, dado seu código.
+     *
+     * @param codCandidato O código do candidato.
+     */
     public void desqualificarCandidato(int codCandidato);
 
+    /**
+     * Este método deve verificar e retornar o status atual de um determinado candidato, dado seu código.
+     *
+     * @param codCandidato O código do candidato.
+     * @return O status atual do candidato.
+     */
     public String verificarStatusCandidato(int codCandidato);
 
+    /**
+     * Este método deve aprovar um determinado candidato no processo, dado seu código.
+     *
+     * @param codCandidato O código do candidato.
+     */
     public void aprovarCandidato(int codCandidato);
 
+    /**
+     * Este método deve retornar uma lista com os nomes de todos os candidatos aprovados.
+     *
+     * @return Uma lista com os nomes dos candidatos aprovados.
+     */
     public List<String> obterAprovados();
 }
--- src/main/java/segundoteste/fases/IFase.java
+++ src/main/java/segundoteste/fases/IFase.java
@@ -4,9 +4,30 @@ import segundoteste.candidatos.Candidato;
 
 import java.util.List;
 
+/**
+ * IFase é uma interface que define os métodos que devem ser implementados pelas fases do processo de seleção de candidatos.
+ */
 public interface IFase {
+
+    /**
+     * Este método deve retornar uma lista de candidatos em uma fase específica.
+     *
+     * @return Uma lista de objetos Candidato.
+     */
     List<Candidato> getCandidatos();
+
+    /**
+     * Este método deve adicionar um novo candidato à fase.
+     *
+     * @param novoCandidato Um objeto Candidato que representa o novo candidato a ser adicionado à fase.
+     */
     void addCandidato(Candidato novoCandidato);
 
+    /**
+     * Este método deve remover um candidato da fase, dado o código do candidato.
+     *
+     * @param codCandidatoToRemove O código do candidato a ser removido.
+     * @return Um objeto Candidato que representa o candidato removido.
+     */
     Candidato removeCandidato(int codCandidatoToRemove);
 }
