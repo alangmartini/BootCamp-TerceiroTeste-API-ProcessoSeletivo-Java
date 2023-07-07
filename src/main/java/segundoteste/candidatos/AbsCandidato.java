5d079e69f6999252e37eef7afe9e39d032343b61
gmaralan <gmartinialan@gmail.com>
2023-07-07 06:25:38 -0400

    in-progress: test Segundo

--- src/main/java/segundoteste/candidatos/AbsCandidato.java
+++ src/main/java/segundoteste/candidatos/AbsCandidato.java
@@ -1,13 +1,33 @@
 package segundoteste.candidatos;
 
-import lombok.Getter;
-import lombok.Setter;
 import segundoteste.fases.EnumFase;
 
-@Getter
-@Setter
 public abstract class AbsCandidato {
     private EnumFase faseAtual;
     private int codCandidato;
     private String nome;
+
+    public EnumFase getFaseAtual() {
+        return faseAtual;
+    }
+
+    public void setFaseAtual(EnumFase faseAtual) {
+        this.faseAtual = faseAtual;
+    }
+
+    public int getCodCandidato() {
+        return codCandidato;
+    }
+
+    public void setCodCandidato(int codCandidato) {
+        this.codCandidato = codCandidato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
