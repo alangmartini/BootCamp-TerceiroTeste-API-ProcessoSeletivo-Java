5228d320f39a723bb09af92c74d78e94fa5bd636
gmaralan <gmartinialan@gmail.com>
2023-07-07 06:13:45 -0400

    chore: candidato implementation

---
+++ src/main/java/segundoteste/candidatos/Candidato.java
@@ -0,0 +1,17 @@
+package segundoteste.candidatos;
+
+
+import segundoteste.fases.EnumFase;
+import segundoteste.fases.IFase;
+import segundoteste.fases.Recebidos;
+
+public  class Candidato extends AbsCandidato {
+    public Candidato(
+            String nome,
+            int codCandidato
+    ) {
+        this.setNome(nome);
+        this.setCodCandidato(codCandidato);
+        this.setFaseAtual(EnumFase.Recebidos);
+    }
+}
