6121bdf6c822a50ffc9b7a762b18a548deb34f77
gmaralan <gmartinialan@gmail.com>
2023-07-07 10:10:51 -0400

    docs: javadocs for Candidato class

--- src/main/java/segundoteste/candidatos/Candidato.java
+++ src/main/java/segundoteste/candidatos/Candidato.java
@@ -1,11 +1,18 @@
 package segundoteste.candidatos;
 
+/**
+ * Candidato é uma classe que estende AbsCandidato.
+ * Esta classe representa um candidato específico no sistema.
+ */
+public class Candidato extends AbsCandidato {
 
-import segundoteste.fases.EnumFase;
-import segundoteste.fases.IFase;
-import segundoteste.fases.Recebidos;
-
-public  class Candidato extends AbsCandidato {
+    /**
+     * Construtor para a classe Candidato.
+     * Este construtor inicializa um novo Candidato com um nome e um código especificados.
+     *
+     * @param nome O nome do candidato como uma String.
+     * @param codCandidato O código do candidato como um int.
+     */
     public Candidato(
             String nome,
             int codCandidato
