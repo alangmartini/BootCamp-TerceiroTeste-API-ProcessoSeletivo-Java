54637cd017f99d4d5681b01952a2be66c24e1e9a
gmaralan <gmartinialan@gmail.com>
2023-07-10 18:00:27 -0400

    test: StartRouteTests update

--- src/test/java/org/ibmBootCamp/terceiroTeste/StartRouteTests.java
+++ src/test/java/org/ibmBootCamp/terceiroTeste/StartRouteTests.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,12 +24,15 @@ class StartRouteTests {
 	void testSuccesful() throws Exception {
 		String requestBody = "{ \"nome\": \"Fulano de tal\" }";
 
+		Map<String, Integer> responseBody = new HashMap<>();
+		responseBody.put("id", 1);
+
 		mockMvc.perform(
 				post("/api/v1/hiring/start")
 					.contentType(MediaType.APPLICATION_JSON)
 					.content(requestBody))
 				.andExpect(status().isCreated())
-				.andExpect(content().string("1"));
+				.andExpect(content().string("{\"id\":1}"));
 	}
 
 	void testNomeInvalidoStringVazia() throws Exception {
