0b0d542cd3f0023118fe5edbe041267ac21c0b9f
gmaralan <gmartinialan@gmail.com>
2023-07-10 16:49:55 -0400

    tdd: POST /api/v1/hiring/start tests

---
+++ src/test/java/org/ibmBootCamp/terceiroTeste/StartRouteTests.java
@@ -0,0 +1,71 @@
+package org.ibmBootCamp.terceiroTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class StartRouteTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+	@Test
+	void testSuccesful() throws Exception {
+		String requestBody = "{ \"nome\": \"Fulano de tal\" }";
+
+		mockMvc.perform(
+				post("/api/v1/hiring/start")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody))
+				.andExpect(status().isCreated())
+				.andExpect(content().string("1"));
+	}
+
+	void testNomeInvalidoStringVazia() throws Exception {
+		String requestBody = "{ \"nome\": \"\" }";
+
+		mockMvc.perform(
+				post("/api/v1/hiring/start")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody))
+			.andExpect(status().isBadRequest())
+			.andExpect(content().string("Nome inválido"));
+	}
+
+	void testNomeInvalidoSemCorpo() throws Exception {
+		String requestBody = "{ }";
+
+		mockMvc.perform(
+				post("/api/v1/hiring/start")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody))
+			.andExpect(status().isBadRequest())
+			.andExpect(content().string("Nome inválido"));
+	}
+
+	void testCandidatoDuplicado() throws Exception {
+		String requestBody = "{ \"nome\": \"Fulano de tal\" }";
+
+		mockMvc.perform(
+				post("/api/v1/hiring/start")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody))
+			.andExpect(status().isOk())
+			.andExpect(content().string("1"));
+
+		mockMvc.perform(
+				post("/api/v1/hiring/start")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody))
+			.andExpect(status().isBadRequest())
+			.andExpect(content().string("Candidato Duplicado"));
+	}
+}
