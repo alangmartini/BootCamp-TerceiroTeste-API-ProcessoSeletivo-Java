a66381585f8d8242fd6db8225db690ea2523227c
gmaralan <gmartinialan@gmail.com>
2023-07-12 12:43:01 -0400

    chore: new error constants for tests

---
+++ src/test/java/org/ibmBootCamp/terceiroTeste/processManagerTests/expectedMessages/ErrorMessages.java
@@ -0,0 +1,15 @@
+package org.ibmBootCamp.terceiroTeste.processManagerTests.expectedMessages;
+
+public class ErrorMessages {
+	static final String CANDIDATO_DUPLICADO = "{\n" +
+		"  \"data\": \"\",\n" +
+		"  \"message\": \"Candidato ja participa do processo.\"\n" +
+		"}";
+
+	static final String NOME_INVALIDO = "{\n" +
+		"  \"data\": \"\",\n" +
+		"  \"message\": \"Nome invalido.\"\n" +
+		"}";
+}
+
+
---
+++ src/test/java/org/ibmBootCamp/terceiroTeste/processManagerTests/expectedMessages/SucessMessages.java
@@ -0,0 +1,10 @@
+package org.ibmBootCamp.terceiroTeste.processManagerTests.expectedMessages;
+
+public class SucessMessages {
+	static final String START_PROCESS = "{\n" +
+		"  \"data\": {\n" +
+		"    \"codCandidato\": 1\n" +
+		"  },\n" +
+		"  \"message\": \"\"\n" +
+		"}";
+}
